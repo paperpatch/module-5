6e210ef20e7cb044baad50eb68bce5aae137bdf8
paperpatch <patchen21@gmail.com>
2021-10-17 18:55:41 -0400

    Schedule Task Auditing

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,8 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  console.log(taskEl)
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -285,6 +287,12 @@ $("#remove-tasks").on("click", function() {
   console.log(tasks);
   saveTasks();
 });
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
